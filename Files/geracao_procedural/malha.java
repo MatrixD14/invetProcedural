ce24e8e74e7f8758131f27f17f3a87d259e8563b
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-08-26 22:20:00 -0400

    buffer up add

--- Files/geracao_procedural/malha.java
+++ Files/geracao_procedural/malha.java
@@ -1,24 +1,37 @@
 public class malha {
   private PerlinNoise perlin = new PerlinNoise(100);
 
-  public void triangulo(int value, int[] triang) {
-    int tris = 0, verts = 0;
-    for (int z = 0; z < value; z ++) {
-      for (int x = 0; x < value; x ++) {
-        triang[tris + 0] = verts + 0;
-        triang[tris + 1] = verts + value + 1;
-        triang[tris + 2] = verts + 1;
-        triang[tris + 3] = verts + 1;
-        triang[tris + 4] = verts + value + 1;
-        triang[tris + 5] = verts + value + 2;
+  public void ChunckSimul(chunkSimul data) {
+    data.VertecesCount += 1;
+    data.NormalCount += 1;
+    data.TrianCount += 6;
+    data.UvMapCount += 1;
+  }
+
+  public void WaterSimul(chunkSimul data) {
+    data.VertecesWaterCount += 1;
+    data.NormalWaterCount += 1;
+    data.TrianWaterCount += 6;
+    data.UvMapWaterCount += 1;
+  } 
+
+  public void trianguloN(int value, IntBuffer triang) {
+    int verts = 0;
+    for (int z = 0; z < value; z++) {
+      for (int x = 0; x < value; x++) {
+        triang.put(verts + 0);
+        triang.put(verts + value + 1);
+        triang.put(verts + 1);
+        triang.put(verts + 1);
+        triang.put(verts + value + 1);
+        triang.put(verts + value + 2);
         verts++;
-        tris += 6;
-      } 
+      }
       verts++;
     }
   }
 
-  public Vertex meshup(boolean onoff, ModelRenderer model, MaterialFile mateFile, int[] trianglo, List<Vector3> vertices, List<Vector3> normal, List<Vector2> uv) {
+  public Vertex meshupN(boolean onoff, ModelRenderer model, MaterialFile mateFile, int[] trianglo, Vector3Buffer vertices, Vector3Buffer normal, Vector2Buffer uv) {
     Vertex vertexs = new Vertex();
     vertexs.setVertices(vertices);
     vertexs.setNormals(normal);
@@ -35,7 +48,7 @@ public class malha {
 
   public float perlinnoises(chunkgen tama, SpatialObject myObj, float x, float z) {
     float calcu = 0;
-    float pi = 22 / 7;
+    float pi = 22f / 7f;
     float valuex = (x + tama.seed) + myObj.position.x;
     float valuez = (z + tama.seed) + myObj.position.z;
     calcu += perlin.noise(valuex, valuez) * tama.heightscale;
