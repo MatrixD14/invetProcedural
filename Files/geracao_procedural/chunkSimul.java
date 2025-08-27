e0f92b4bed7b41ef2a4c821ae40c0608f24220be
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-08-26 23:42:11 -0400

    buffer up add how success

--- Files/geracao_procedural/chunkSimul.java
+++ Files/geracao_procedural/chunkSimul.java
@@ -1,21 +1,22 @@
 public class chunkSimul {
-  public int VertecesCount, NormalCount, TrianCount,UvMapCount;
-  public int VertecesWaterCount, NormalWaterCount, TrianWaterCount,UvMapWaterCount;
+  public int VertecesCount, NormalCount, TrianCount, UvMapCount;
+  public int VertecesWaterCount, NormalWaterCount, TrianWaterCount, UvMapWaterCount;
 
-  public void generat(int value, int[][] block, float[][] heigth, malha data) {
+  public void generat(int value, OH2LevelIntArray block, OH2LevelFloatArray heigth, malha data) {
     for (int z = 0; z <= value; z++) {
       for (int x = 0; x <= value; x++) {
-        float y = heigth[z][x];
-        int matriz = block[z][x];
+        float y = heigth.get(z, x);
+        int matriz = block.get(z, x);
         data.ChunckSimul(this);
-      } 
-    }
+      }
+    } 
   }
+
   public void generatWater(int value, malha data) {
     for (int z = 0; z <= value; z++) {
       for (int x = 0; x <= value; x++) {
         data.WaterSimul(this);
-      } 
+      }
     }
   }
 }
