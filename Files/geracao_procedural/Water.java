5e04e7efa21ca8b6eba4d5f7885103b0c27e53d1
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-08-29 07:54:17 -0400

    upload do script que estao funcionando

--- Files/geracao_procedural/Water.java
+++ Files/geracao_procedural/Water.java
@@ -1,53 +1,33 @@
 public class Water extends Component {
   private ModelRenderer WaterModelo;
-  private Vertex WaterVertex;
-  private Vector2Buffer WaterUV = null;
-  private Vector3Buffer WaterVertices = null, WaterNormal = null;
-  private IntBuffer WaterTriangle = null;
-  private chunkgen tama;
-  private malha modela = new malha();
-
+  private chunkgen tama = new chunkgen();
   void start() {
     if (!myObject.exists()) return;
     tama = WorldController.findObject("player").findComponent("chunkgen");
     WaterModelo = myObject.findComponent("modelrenderer");
-    WaterVertex = new Vertex();
   }
 
   public void WaterGera() {
     start();
-    createBuffer();
-    int mult = 1 * 16;
-    geraVerteces(mult, mult);
-    geraVerteces(0, mult);
-    geraVerteces(mult, 0);
-    geraVerteces(0, 0);
+    Vertex WaterVertex = new Vertex();
+    float y = tama.waterlevel;
+    int mult = 16;
+    float[] WaterVertices = new float[] {0, y, 0, 0, y, mult, mult, y, 0, mult, y, mult};
+    float[] WaterNormal = new float[] {0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0};
+    float[] WaterUV = new float[] {0, 0, 0, 1, 1, 0, 1, 1};
+    int[] WaterTriangle = new int[] {0, 1, 2, 2, 1, 3};
     
-    modela.trianguloN(1, WaterTriangle);
-    int[] WaterTriangleConvert = new int[WaterTriangle.position()];
-    WaterTriangle.rewind();
-    WaterTriangle.get(WaterTriangleConvert);
-    WaterVertex = modela.meshupN(false, WaterModelo, tama.WaterMate, WaterTriangleConvert, WaterVertices, WaterNormal, WaterUV);
-    myObject.removeComponent(new Water());
-  }
+    WaterVertex.setVertices(WaterVertices);
+    WaterVertex.setNormals(WaterNormal);
+    WaterVertex.setUVs(WaterUV);
+    WaterVertex.setTriangles(WaterTriangle);
+    WaterVertex.apply();
 
-  private void geraVerteces(int x, int z) {
-    WaterVertices.put(x, tama.waterlevel, z);
-    WaterNormal.put(0, 1, 0);
-    WaterUV.put(new Vector2(x, z));
-  }
+    if (tama.WaterMate != null) WaterModelo.setMaterialFile(tama.WaterMate);
 
-  public void createBuffer() {
-    chunkSimul data = new chunkSimul();
-    boolean offon = false;
-    data.generatWater(1, modela);
-    WaterVertices = BufferUtils.createVector3Buffer(data.VertecesWaterCount);
-    WaterNormal = BufferUtils.createVector3Buffer(data.NormalWaterCount);
-    WaterTriangle = BufferUtils.createIntBuffer(data.TrianWaterCount);
-    WaterUV = BufferUtils.createVector2Buffer(data.UvMapWaterCount);
-    if (!offon) return;
-    WaterVertices.setVboEnabled(true);
-    WaterNormal.setVboEnabled(true);
-    WaterUV.setVboEnabled(true);
-  }
+    WaterModelo.material.setReceiveLight(false);
+
+    if (WaterVertex != null) WaterModelo.setVertex(WaterVertex);
+    WaterModelo.setCastShadowEnabled(false);
+  } 
 }
